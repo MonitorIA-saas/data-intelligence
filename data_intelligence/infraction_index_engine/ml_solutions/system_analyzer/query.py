7d197af5faa42fcab3f45043e1f7e085c0f43ab2
eduardocostaam <eduardocosta942z@gmail.com>
2026-01-02 20:02:15 -0300

    feat: basic query offer

---
+++ data_intelligence/infraction_index_engine/ml_solutions/system_analyzer/query.py
@@ -0,0 +1,13 @@
+from data_intelligence.models.Process import Process
+from data_intelligence.infraction_index_engine.ml_solutions.system_analyzer.model import create_model, train
+import os
+
+def predict(checkpoint_path, process: Process):
+    if not os.path.exists(checkpoint_path):
+        model_info = create_model()
+    else:
+        model_info = create_model(checkpoint_path)
+    
+    train(model_info)
+    model_info[0].predict([process])
+    
